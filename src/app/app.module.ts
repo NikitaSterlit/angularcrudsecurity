58f33ca00f1be81272fd88939afafbd1ee67dd93
Nikita <klerik1995@mail.ru>
2019-05-20 22:39:17 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistrComponent } from './registr/registr.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { AuthComponent } from './auth/auth.component';
+import {LoginService} from './auth/loginservice';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
